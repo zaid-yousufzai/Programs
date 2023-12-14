a17c4984be582c31c81ef7208a2b40d0346cc60b
zaid-yousufzai <zaidkhansahab5@gmail.com>
2023-12-15 01:16:20 +0530

    third

--- src/patterns/Pattern19.java
+++ src/patterns/Pattern19.java
@@ -10,11 +10,12 @@ package patterns;
  *
  * @author Hp
  */
-public class Pattern18 {
+public class Pattern19 {
+    
     
     public static void main(String args[])
     {
-        for(int i=1;i<=5;i++)
+       for(int i=1;i<=5;i++)
         {
             for(int j=5;j>i;j--)
             {
@@ -22,13 +23,21 @@ public class Pattern18 {
             }
             for(int j=1;j<=5;j++)
             {
-                System.out.print("@");
+                
+                if(i==1 || i==5  || j==1 || j==5)
+                {
+                      System.out.print("@");
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+              
             }
             System.out.println();
            
         }
         
-        
-      
     }
+    
 }
